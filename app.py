63ab2c1d5103871dc33b15d6c4f93102366885af
Donatellh2o <104277659+Donatellh2o@users.noreply.github.com>
2023-02-28 18:39:53 +0100

    deletion of page.png after sending

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask_api import FlaskAPI
 from flask import send_file, request
 import fitz
+import os
 
 app = FlaskAPI(__name__)
 
@@ -92,12 +93,15 @@ def generate():
         if int(key) in range(0, 10):
             generate_number(rgb, key, 0)
             return send_file('page.png', mimetype="image/png")
+            os.remove("page.png")
     except:
         print()
 
     if key in ['sauter', 'sens', 'plus_2', 'changer', 'plus_4']:
         generate_number(rgb, key, 1)
         return send_file('page.png', mimetype="image/png")
+        os.remove("page.png")
+    
 
     if key == 'complete':
         return send_file('result.pdf', mimetype="image/pdf")
